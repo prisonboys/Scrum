e066dbc2c574bfee1be08b9baccf7dd41be9ac54
Kevin Weiß <superprisonboys@gmail.com>
2016-05-13 15:15:37 +0200

    Schiebe GUI

---
+++ UnityProjekt/Assets/GUISchieben.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GUISchieben : MonoBehaviour {
+
+
+	public GameObject pfeil;
+
+
+	void OnTriggerStay (Collider other) 
+	{
+		if (other.tag == "Player") {
+			pfeil.active = true; 
+		}
+	}
+	void OnTriggerExit (Collider other)
+	{
+		if (other.tag == "Player") {
+			pfeil.active = false;
+		}
+	}
+}
