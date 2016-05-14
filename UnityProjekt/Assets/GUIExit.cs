98b6f5f220c806a36a8454e06d37ec55eb1401a2
Kevin Weiß <superprisonboys@gmail.com>
2016-05-14 14:47:57 +0200

    GUI

---
+++ UnityProjekt/Assets/GUIExit.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GUIExit : MonoBehaviour {
+
+	public GameObject symbol;
+
+
+	void OnTriggerStay (Collider other) 
+	{
+		if (other.tag != "Player") {
+			symbol.active = false; 
+		}
+	}
+	void OnTriggerExit (Collider other)
+	{
+		if (other.tag != "Player") {
+			symbol.active = true;
+		}
+	}
+}
