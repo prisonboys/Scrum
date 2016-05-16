ec8d20d3aa26c0247f78bea56a4d24701c93c814
Kevin Weiß <superprisonboys@gmail.com>
2016-05-16 13:01:12 +0200

    Titlescreen

---
+++ UnityProjekt/Assets/Scripts/GUI Skripte/LoadLevel.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadLevel : MonoBehaviour 
+{ 
+	public void LoadScene(string sceneName) 
+	{ 
+		Application.LoadLevel(sceneName); 
+	} 
+}
---
+++ UnityProjekt/Assets/Scripts/GUI Skripte/PressEnterGUI.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PressEnterGUI : MonoBehaviour 
+{
+	public GameObject Title;
+	public GameObject Menu;
+
+	void Update () 
+	{
+		if (Input.anyKey) 
+		{
+			Title.active = false;
+			Menu.active = true;
+		}	
+	}
+}
