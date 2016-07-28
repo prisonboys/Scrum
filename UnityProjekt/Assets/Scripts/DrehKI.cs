fc1c7d44d00fb9680007557a67058c4ed5544b84
Kevin Weiß <superprisonboys@gmail.com>
2016-07-28 14:17:10 +0200

    Update

---
+++ UnityProjekt/Assets/Scripts/DrehKI.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DrehKI : MonoBehaviour {
+    public float Drehgeschwindigkeit = 10f;
+    public GameObject KI;
+    public bool Drehrichtung = false; //false heißt auf 90grad zubewegen true heißt auf 270//
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    // Update is called once per frame
+    void Update() {
+        if (Drehrichtung == false)
+        {
+            if (transform.rotation.eulerAngles.y < 90)
+            {
+                Drehrichtung = true;
+
+            }
+        }
+        if (Drehrichtung == true)
+        {
+            if (transform.rotation.eulerAngles.y > 270)
+            {
+                Drehrichtung = false;
+            }
+        }
+
+        if (Drehrichtung == false) {
+            transform.Rotate(Vector3.up, Drehgeschwindigkeit * Time.deltaTime * -1);
+            
+
+        }
+        else
+        {
+            transform.Rotate(Vector3.up, Drehgeschwindigkeit * Time.deltaTime);
+
+        }
+	}
+}
---
+++ UnityProjekt/Assets/Scripts/Levelwechsel.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Levelwechsel : MonoBehaviour {
+    public string NeuesLevel;
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    void OnTriggerEnter (Collider other)
+    {
+        Application.LoadLevel(NeuesLevel);
+    }
+}
